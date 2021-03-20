d2adb7c7c6c053a1e9dc0e4e0dd8af37e3e9b06e
B-63Vikas <80041195+B-63Vikas@users.noreply.github.com>
2021-03-19 23:06:31 -0700

    Greatest integer among 3 numbers

---
+++ Maximum of 3 numbes.py
@@ -0,0 +1,9 @@
+N1=int(input("Enter 1st No.: "))
+N2=int(input("Enter 2nd No.: "))
+N3=int(input("Enter 3rd No.: "))
+if N1> N2 and N3:
+    print(N1, "is greatest of all")
+elif N2> N3 and n1:
+    print(N2, "is greatest of all")
+else:
+    print(N3, "is greatest of all")
